c4a8ed721683b5d9b3f872a6fb04db0e8fe03c73
Fabian Kozynski <kozynski@google.com>
2020-09-24 12:09:32 -0400

    SystemUI: Add Executors to Dependency
    wave notes:
    - fixes assistant keyguard affordance icon not working
    
    Test: manual, trigger KeyguardBottomAreaView#launchVoiceAssist
    Fixes: 167421921
    Change-Id: I9b366f2fb69f47e0fb7bf19ced0c5382d0bb9c7c
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -93,8 +93,6 @@ import com.android.systemui.tuner.LockscreenFragment.LockButtonFactory;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.volume.SystemUIInterpolators;
 
-import java.util.concurrent.Executor;
-
 /**
  * Implementation for the bottom area of the Keyguard, including camera/phone affordance and status
  * text.
@@ -645,7 +643,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             }
         };
         if (!mKeyguardStateController.canDismissLockScreen()) {
-            Dependency.get(Executor.class).execute(runnable);
+            Dependency.get(Dependency.BACKGROUND_EXECUTOR).execute(runnable);
         } else {
             boolean dismissShade = !TextUtils.isEmpty(mRightButtonStr)
                     && Dependency.get(TunerService.class).getValue(LOCKSCREEN_RIGHT_UNLOCK, 1) != 0;
